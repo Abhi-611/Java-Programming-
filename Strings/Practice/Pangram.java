3e6fc93303ae36cc09be233c01825c60d94d90ce
Abhi-611 <badadeabhi2002@gmail.com>
2023-07-13 14:20:51 +0530

    commit

--- Strings/Practice/Pangram.java
+++ Strings/Practice/Pangram.java
@@ -1,5 +1,31 @@
 package Strings.Practice;
 
 public class Pangram {
-    
+    public static void main(String[] args) {
+        boolean flag = false;
+        String str = "THE QUICK BROWN FOX JUMPS OVER LAZY DOG";
+
+        str = str.replace(" ","");
+        char []ch = str.toCharArray();
+
+        int ar[] = new int[26];
+
+        for(int i=0;i<ch.length;i++)
+        {
+            ar[ch[i]-65]++;
+        }
+        for(int i=0;i<ar.length;i++)
+        {
+           if(ar[i]==0)
+           {
+            System.out.println("It is not Pangram");
+            flag = true;
+           }
+        }
+        if(flag==false)
+        {
+            System.out.println("It is  Pangram");
+
+        }
+    }
 }
